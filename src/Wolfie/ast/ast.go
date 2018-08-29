0f4b4653873bb47ab0a7358d95ebb44500c8c9b3
hundredrab <sourab.jha@kgec.edu.in>
2018-08-30 01:08:01 +0530

    Add AST initially.

---
+++ src/Wolfie/ast/ast.go
@@ -0,0 +1,19 @@
+import "Wolfie/token"
+
+type LetStatement struct {
+    Token token.Token
+    Name *Identifier
+    Calue Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
